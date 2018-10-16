b35d84f55a59f9bce7ccb75df373f04087fe2e7a
OBrooks <mr.willh@gmail.com>
2018-10-16 09:59:06 +0200

    board

---
+++ ttt.rb
@@ -0,0 +1,43 @@
+class Board
+    def initialize
+        A1 = Boardcases.new
+        A2 = Boardcases.new
+        A3 = Boardcases.new
+        B1 = Boardcases.new
+        B2 = Boardcases.new
+        B3 = Boardcases.new
+        C1 = Boardcases.new
+        C2 = Boardcases.new
+        C3 = Boardcases.new
+    end
+
+
+end
+
+
+class Boardcases
+
+
+
+
+end
+
+
+class Player
+
+end
+
+
+class Game
+
+end
+
+Game.new.go
+
+puts  "+---+---+---+"
+puts  "| 1 | 2 | 3 |"
+puts  "+---+---+---+"
+puts  "| 4 | 5 | 6 |"
+puts  "+---+---+---+"
+puts  "| 7 | 8 | 9 |"
+puts  "+---+---+---+"
